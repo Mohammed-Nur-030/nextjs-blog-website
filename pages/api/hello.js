26fa3f70607044f4c8d71853f3cfeb0436f25457
Mohammed-Nur-030 <126596779+Mohammed-Nur-030@users.noreply.github.com>
2024-01-03 21:33:30 +0530

    hello api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,9 +1,19 @@
 // // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// const fs = require('node:fs');
-// export default function handler(req, res) {
+const fs = require('node:fs');
+export default function handler(req, res) {
   
-//   fs.readFile("../../blogData/how-to-learn-javascript.json",()=>{
-//     console.log(data);
-//   })
-//   res.status(200).json({ name: 'John Doe' })
-// }
+    const currentDate = new Date();
+    const formattedDate = new Intl.DateTimeFormat('en-US', {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      timeZoneName: 'short',
+    }).format(currentDate);
+    
+    console.log(formattedDate);
+  console.log("date",formattedDate);
+  res.status(200).json({ formattedDate })
+}
